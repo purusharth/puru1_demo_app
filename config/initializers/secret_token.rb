359a75b1d92f23f7160b2397a8b0c525473f64e2
Purusharth Prakash <puru1@umbc.edu>
2014-02-06 18:35:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Puru1DemoApp::Application.config.secret_key_base = 'a5e7cac2f6896ebd2b7d6373795cf84a044409b2e9ccf29369f61da3b2b515891b7e431543917db28cbbf5cc48160e8e9afbcda56cb4046add8803752623188f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Puru1DemoApp::Application.config.session_store :cookie_store, key: '_puru1_demo_app_session'
